a8619499977b706166f0ab1b5f22f1d0f22841d0
Mark McCarthy <mm3guides@gmail.com>
2024-08-20 15:26:55 +0100

    challenge solution

--- src/controllers/changePassword.js
+++ src/controllers/changePassword.js
@@ -1,5 +1,31 @@
-async function updatePassword(params) {
-    
+const User = require("../db/models/users");
+const bcrypt = require("bcrypt");
+
+async function updatePassword(req,res) {
+    //User.update(
+   //{password: req.body.hashedNewPassword},
+  // {where:{ User.email : req.body.email}}
+  try {
+    saltRounds = parseInt(process.env.SALT_ROUNDS);
+    const hashedNewPassword = await bcrypt.hash(req.body.newPassword, saltRounds);
+    console.log(hashedNewPassword)
+    const result = await User.update({password: hashedNewPassword},{
+      where: {
+        email: req.body.email
+      }
+    })
+    console.log(result);
+    res.status(200).json({
+      message: "Password updated",
+      results: result
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "oops password njot updated",
+      errorMessage: error
+    })
+  }
 }
 
 module.exports = updatePassword
--- src/controllers/deleteUser.js
+++ src/controllers/deleteUser.js
@@ -1,5 +1,21 @@
-async function deleteUser(params) {
-    
+const User = require("../db/models/users");
+
+async function deleteUser(req,res) {
+    try {
+        const result = await User.destroy({
+            where: {
+                email: req.body.email
+            }
+        })
+        console.log(result);
+        res.status(200).send(`User ${req.body.email} deleted`);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "delete failed",
+            errorMessage: error
+        })
+    }
 }
 
 module.exports = deleteUser
